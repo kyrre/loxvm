4bf679a2b7bd2e286495a161b2b72e4fae9e02df
Kyrre Wahl Kongsgård <kyrre.wahl.kongsgaard@gmail.com>
2022-11-12 14:42:30 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use loxvm::vm::VM;
 
 fn main() {
     
-    
     let mut vm = VM::new();
-
     let mut chunk = Chunk::new();
     let constant = chunk.add_constant(1.2);
     chunk.write_chunk(OpCode::OP_CONSTANT(constant), 123);
